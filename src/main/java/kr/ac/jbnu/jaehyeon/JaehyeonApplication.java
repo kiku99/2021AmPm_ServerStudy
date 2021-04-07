134eb168afc449a6b20c2ea1620dbd8f4e38441d
kiku99 <elsd0326@gmail.com>
2021-04-07 23:03:32 +0900

    first init

---
+++ src/main/java/kr/ac/jbnu/jaehyeon/JaehyeonApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jbnu.jaehyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaehyeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaehyeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/jbnu/jaehyeon/JaehyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jbnu.jaehyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaehyeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
